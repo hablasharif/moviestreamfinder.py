58aaea52cbf004d02b47c3d9253bbf74d3e5bd08
hablasharif <138958393+hablasharif@users.noreply.github.com>
2023-09-23 11:52:40 +0600

    Update moviestreamfinder.py

--- moviestreamfinder.py
+++ moviestreamfinder.py
@@ -1,9 +1,9 @@
 import streamlit as st
 from imdb import IMDb
+import requests
 import pandas as pd
 from tqdm import tqdm  # Import tqdm for the progress bar
 from bs4 import BeautifulSoup
-import requests
 
 # Streamlit App Title
 st.title("Movie Search App")
@@ -12,7 +12,7 @@ st.title("Movie Search App")
 st.sidebar.header("Movie Details")
 uploaded_file = st.sidebar.file_uploader("Upload a CSV file with movie details", type=["csv"])
 
-# Function to search for a movie and get IMDb URL
+# Function to search for a movie and get IMDb ID
 def search_movie_imdb(movie_name, release_year):
     ia = IMDb()
     movies = ia.search_movie(movie_name)
@@ -23,8 +23,7 @@ def search_movie_imdb(movie_name, release_year):
     if filtered_movies:
         movie = filtered_movies[0]  # Get the first matching movie
         imdb_id = movie.getID()
-        imdb_url = f"https://www.imdb.com/title/tt{imdb_id}"
-        return imdb_url
+        return imdb_id
     else:
         return None
 
@@ -57,34 +56,30 @@ def generate_vidsrc_url(imdb_id=None, tmdb_id=None):
     else:
         return None
 
-# Function to get the title from a URL, using IMDb ID or TMDb ID as fallback
-def get_title_from_url(vidsrc_url, imdb_id, tmdb_id):
-    try:
-        if "tt" in vidsrc_url:
-            imdb_id = vidsrc_url.split("tt")[-1]
-        elif "tmdb" in vidsrc_url:
-            tmdb_id = vidsrc_url.split("tmdb=")[-1]
+# Function to generate vidsrc URL with IMDb ID
+def generate_vidsrc_url_with_imdb_id(imdb_id):
+    if imdb_id:
+        return f"https://vidsrc.to/embed/movie/tt{imdb_id}"
+    else:
+        return None
 
-        if imdb_id:
-            ia = IMDb()
-            movie = ia.get_movie(imdb_id)
-            return movie.get("title")
-        elif tmdb_id:
-            tmdb_api_key = "b0abe1120c53c9731ee3d32b81dd7df5"
-            base_url = f"https://api.themoviedb.org/3/movie/{tmdb_id}"
-
-            params = {
-                "api_key": tmdb_api_key,
-            }
-
-            response = requests.get(base_url, params=params)
-
-            if response.status_code == 200:
-                data = response.json()
-                return data.get("original_title")
-    except Exception as e:
-        st.warning(f"Error processing movie: {e}")
+# Function to get the movie title from a vidsrc URL
+def get_vidsrc_title(vidsrc_url):
+    if vidsrc_url:
+        response = requests.get(vidsrc_url)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.content, 'html.parser')
+            title_tag = soup.find('title')
+            if title_tag:
+                return title_tag.text.strip()
     return None
+
+# Function to generate IMDb hyperlink
+def generate_imdb_hyperlink(imdb_id):
+    if imdb_id:
+        return f"[https://www.imdb.com/title/tt{imdb_id}](https://www.imdb.com/title/tt{imdb_id})"
+    else:
+        return None
 
 # Main content
 if uploaded_file is not None:
@@ -99,24 +94,27 @@ if uploaded_file is not None:
             movie_name = row['Movie Name']
             release_year = row['Release Year']
 
-            imdb_url = search_movie_imdb(movie_name, release_year)
+            imdb_id = search_movie_imdb(movie_name, release_year)
             tmdb_id = search_movie_tmdb(movie_name, release_year)
-
-            # Generate the vidsrc URL
-            vidsrc_url = generate_vidsrc_url(imdb_url, tmdb_id)
             
-            # Get the title using IMDb ID or TMDb ID as fallback
-            vidsrc_title = get_title_from_url(vidsrc_url, None, tmdb_id)
+            vidsrc_url = generate_vidsrc_url(imdb_id, tmdb_id)
+            imdb_hyperlink = generate_imdb_hyperlink(imdb_id)
+            
+            # Generate the vidsrc URL with IMDb ID
+            vidsrc_url_with_imdb_id = generate_vidsrc_url_with_imdb_id(imdb_id)
+            
+            # Get the vidsrc title
+            vidsrc_title = get_vidsrc_title(vidsrc_url)
 
-            if vidsrc_title is not None:
-                results.append({
-                    'Movie Name': movie_name,
-                    'Release Year': release_year,
-                    'IMDb': imdb_url,
-                    'TMDb ID': tmdb_id,
-                    'vidsrc': vidsrc_url,
-                    'vidsrc_title': vidsrc_title
-                })
+            results.append({
+                'Movie Name': movie_name,
+                'Release Year': release_year,
+                'IMDb': imdb_hyperlink,
+                'TMDb ID': tmdb_id,
+                'vidsrc': vidsrc_url,
+                'vidsrc_title': vidsrc_title,
+                'vidsrc_with_imdb_id': vidsrc_url_with_imdb_id  # Add the IMDb ID in vidsrc URL
+            })
 
             # Update the progress bar
             pbar.update(1)
